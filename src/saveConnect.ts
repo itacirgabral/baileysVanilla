12046822b51e2d797e913b60cdcb90bbf23a32eb
itacirgabral <6052177+itacirgabral@users.noreply.github.com>
2021-12-01 17:36:46 -0400

    Update saveConnect.ts

--- src/saveConnect.ts
+++ src/saveConnect.ts
@@ -1,26 +1,24 @@
 import { initInMemoryKeyStore, BufferJSON, AuthenticationState  } from '@adiwajshing/baileys-md'
+const { readFileSync, writeFileSync, existsSync } = require('fs')
 
 export const saveConnect = (filename: string) => {
-	// require fs here so that in case "fs" is not available -- the app does not crash
-	const { readFileSync, writeFileSync, existsSync } = require('fs')
-
 	let state: AuthenticationState
 
 	const saveState = () => {
-    console.log('saving auth state saveConnect')
-    const toWrite = JSON.stringify(state, BufferJSON.replacer, 2)
+		console.log('saving auth state saveConnect')
+		const toWrite = JSON.stringify(state, BufferJSON.replacer, 2)
 
-    writeFileSync(filename, toWrite)
+		writeFileSync(filename, toWrite)
 	}
 
-  const { creds, keys } = JSON.parse(readFileSync(filename, { encoding: 'utf-8' }), BufferJSON.reviver)
+	const { creds, keys } = JSON.parse(readFileSync(filename, { encoding: 'utf-8' }), BufferJSON.reviver)
 
-  state = { 
-      creds: creds, 
-      // stores pre-keys, session & other keys in a JSON object
-      // we deserialize it here
-      keys: initInMemoryKeyStore(keys, saveState) 
-  }
+	state = { 
+		creds: creds, 
+		// stores pre-keys, session & other keys in a JSON object
+		// we deserialize it here
+		keys: initInMemoryKeyStore(keys, saveState) 
+	}
 
 	return { state, saveState }
 }
